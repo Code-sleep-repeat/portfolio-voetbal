f056d99fac7940aba786856bbe561dd716a8ae91
FatPuppy <mrijper99@gmail.com>
2019-07-01 10:20:34 +0200

    voorpagina

---
+++ v2/homepagina.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/homepage.css">
+</head>
+<body>
+<header>
+
+</header>
+<main>
+    <div id="homepage">
+        <form id="form" method="post">
+            <nav>
+                <p>FCA</p>
+                <ul>
+                    <li><a href="#">Leden</a>
+                        <ul>
+                            <li><a href="index.php">alle leden</a></li>
+                            <li><a href="create.php">nieuw lid</a></li>
+                            <li><a href="update.php">verander lid</a></li>
+                        </ul>
+                    </li>
+        </form>
+    </div>
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
